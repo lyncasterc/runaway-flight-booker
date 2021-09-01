04b2650421ea060f303a53c50b2f25da0d8f6af9
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-08-31 23:26:03 -0400

    write flights index controller

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,12 +2,27 @@ class FlightsController < ApplicationController
 
   def index
     @airports = Airport.all
-    @dates = Flight.all.map(&:start_date).uniq
+    @dates = Flight.pluck(:start_date).uniq
+    @search = params
+
+    if @search.present?
+      flights = Flight.where(start_id: @search[:from_airport], 
+                              finish_id: @search[:to_airport], 
+                              start_date: @search[:start_date])
+
+      @flights_info = flights.map do |flight|
+        { from_airport: Airport.find_by(id: flight.start_id), 
+          to_airport: Airport.find_by(id: flight.finish_id), 
+          start_date: flight.start_date, 
+          duration: flight.flight_duration }
+      end
+    end
+    
   end
 
   private
 
-  def flights_params
-    params.require(:flight).permit(:airport)
-  end
+  # def flights_params
+  #   params.require(:flight).permit(:airport)
+  # end
 end
