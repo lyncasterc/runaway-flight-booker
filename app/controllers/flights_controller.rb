e244f48482f8d55ea197b357db0420827f008b30
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-09-01 01:49:13 -0400

    refactor flights index action

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -5,20 +5,12 @@ class FlightsController < ApplicationController
     @dates = Flight.pluck(:start_date).uniq
     @search = params
     @passenger_count = params[:passenger_count]
-
+    
     if @search.present?
-      flights = Flight.where(start_id: @search[:from_airport], 
+      @flights = Flight.where(start_id: @search[:from_airport], 
                               finish_id: @search[:to_airport], 
                               start_date: @search[:start_date])
-
-      @flights_info = flights.map do |flight|
-        { from_airport: Airport.find_by(id: flight.start_id), 
-          to_airport: Airport.find_by(id: flight.finish_id), 
-          start_date: flight.start_date, 
-          duration: flight.flight_duration }
-      end
     end
-    
   end
 
   private
