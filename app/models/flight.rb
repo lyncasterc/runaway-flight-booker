81855b98a6f8832cab1e568c99630d6922a66759
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-08-31 23:44:47 -0400

    update associations

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,6 +1,6 @@
 class Flight < ApplicationRecord
   belongs_to :from_airport, foreign_key: :start_id, class_name: 'Airport'
   belongs_to :to_airport, foreign_key: :finish_id, class_name: 'Airport'
-  has_many :passengers
+  has_many :passengers, through: :bookings
   has_many :bookings
 end
