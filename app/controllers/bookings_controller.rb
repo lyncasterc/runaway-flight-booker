cd3b81adb93b8a5a33964c432c4d1ace97b7ac12
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-09-05 17:47:28 -0400

    write new, create show actions in bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,37 @@
 class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+    @flight = Flight.find(params[:flight_id])
+    @booking.flight = @flight
+    passenger_count = params[:passenger_count].to_i
+
+    passenger_count.times do 
+      @booking.bookings_passengers.build.build_passenger
+    end
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @flight = Flight.find(params[:flight_id])
+    @booking.flight = @flight
+    respond_to do |format|
+      if @booking.save
+        format.html { redirect_to @booking, notice: "Your flight has been booked!"}
+        format.json { render :show, status: :created, location: @booking }
+      else
+        format.html { render :new }
+        format.json { render json: @booking.errors, status: :unproccessable_entity }
+      end
+    end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:flight_id, bookings_passengers_attributes: [passenger_attributes: [:name, :email]])
+  end
 end
