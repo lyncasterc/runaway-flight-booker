9ff63adc42b7f802a9969eefff596658e932d150
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-09-05 17:48:34 -0400

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :flights, only: [:index, :show] do
-    resources :bookings, only: [:new, :create, :show]
+    resources :bookings, only: [:new, :create] 
   end
+  
+  resources :bookings, only: [:show]
   root 'flights#index'
 end
